50651acedd8e8dcd711f57643649f5e99c2ddc3b
Artem Antufiev <aaartem70495.trash@gmail.com>
2021-08-26 14:30:34 +0300

    feat(rest): Add available quantity field

--- rest/src/main/java/com/example/eshop/rest/resources/catalog/SkuResource.java
+++ rest/src/main/java/com/example/eshop/rest/resources/catalog/SkuResource.java
@@ -6,9 +6,11 @@ import com.example.eshop.rest.resources.shared.MoneyResource;
 public class SkuResource {
     public String ean;
     public MoneyResource price;
+    public int quantity;
 
     public SkuResource(Sku sku) {
         this.ean = sku.getEan().toString();
         this.price = new MoneyResource(sku.getPrice());
+        this.quantity = sku.getAvailableQuantity();
     }
 }
