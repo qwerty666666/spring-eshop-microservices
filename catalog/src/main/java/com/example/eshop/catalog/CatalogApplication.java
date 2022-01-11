775c30c292a431e3d1f75698e1186fce50c2020c
Artem Antufiev <aaartem70495.trash@gmail.com>
2022-01-12 02:16:20 +0300

    refactor: use spring-boot plugin for independent builds
    - use different "dev" profile names as we have modules dependent on others with the same application-dev property file
    - use spring-boot-maven-plugin for build self-contained microservice jars

--- catalog/src/main/java/com/example/eshop/catalog/CatalogApplication.java
+++ catalog/src/main/java/com/example/eshop/catalog/CatalogApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
-public class App {
+public class CatalogApplication {
     public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
+        SpringApplication.run(CatalogApplication.class, args);
     }
 }
