45f95b2d2203dbbca11497facb71914bd5336bbd
Artem Antufiev <aaartem70495.trash@gmail.com>
2022-01-27 22:11:26 +0300

    feat: add service discovery (Eureka)

--- api-gateway/src/main/java/com/example/eshop/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/example/eshop/apigateway/ApiGatewayApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ApiGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
@@ -25,13 +27,13 @@ public class ApiGatewayApplication {
                                         .setMaxValue(30)
                                 ))
                         )
-                        .uri("http://localhost:8087")
+                        .uri("lb://catalog-service")
 
                 )
                 .route("monolith", r -> r
                         .order(1000)
                         .path("/**")
-                        .uri("http://localhost:8088")
+                        .uri("lb://monolith")
                 )
                 .build();
     }
--- rest/src/main/java/com/example/eshop/rest/RestApplication.java
+++ rest/src/main/java/com/example/eshop/rest/RestApplication.java
@@ -3,9 +3,11 @@ package com.example.eshop.rest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication(scanBasePackages = "com.example.eshop")
 @EnableConfigurationProperties({ AppProperties.class })
+@EnableEurekaClient
 public class RestApplication {
     public static void main(String[] args) {
         SpringApplication.run(RestApplication.class, args);
