8627dd361f32495d5cdfd5d285583d105ea4336c
Artem Antufiev <aaartem70495.trash@gmail.com>
2022-02-23 03:09:49 +0300

    fix(catalog): fix attributes sorting
    The comparator used for keys in TreeMap and therefore elements for which comparator return 0 will be skipped

--- catalog-service/catalog-server/src/main/java/com/example/eshop/catalog/domain/product/AttributesComparator.java
+++ catalog-service/catalog-server/src/main/java/com/example/eshop/catalog/domain/product/AttributesComparator.java
@@ -5,6 +5,10 @@ import java.util.Comparator;
 public class AttributesComparator implements Comparator<AttributeValue> {
     @Override
     public int compare(AttributeValue o1, AttributeValue o2) {
-        return o1.getAttribute().getName().compareTo(o2.getAttribute().getName());
+        if (o1.equals(o2)) {
+            return 0;
+        }
+
+        return o1.getAttribute().getName().compareTo(o2.getAttribute().getName()) > 0 ? 1 : -1;
     }
 }
