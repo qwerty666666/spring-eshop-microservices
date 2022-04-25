2a6146fcb81733da996511786673b4dafd497512
Artem Antufiev <aaartem70495.trash@gmail.com>
2022-04-26 01:45:07 +0300

    refactor(docker): docker dir layout

--- init-and-run.sh
+++ init-and-run.sh
@@ -1,10 +1,10 @@
 ./scripts/mvn-install.sh
 ./scripts/build-images.sh
 
-cd docker
+cd docker/services
 
 docker-compose \
-    --env-file .env.init-test-data \
+#    --env-file .env.init-test-data \
     -f docker-compose.yml \
     -f docker-compose.init.yml \
     up -d
