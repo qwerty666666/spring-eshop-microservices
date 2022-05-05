6c48c50d340443244a00df39152e6e94188ad049
Artem Antufiev <aaartem70495.trash@gmail.com>
2022-05-05 17:56:08 +0300

    feat(elk): collect logs to elk only for "elk-logs" profile

--- init-and-run.sh
+++ init-and-run.sh
@@ -1,12 +1,9 @@
 ./scripts/mvn-install.sh
 ./scripts/build-images.sh
 
-cd docker/services
-
 docker-compose \
 #    --env-file .env.init-test-data \
-    -f docker-compose.yml \
-    -f docker-compose.init.yml \
+    -f docker/elk/docker-compose.yml \
+    -f docker/services/docker-compose.yml \
+    -f docker/services/docker-compose.init.yml \
     up -d
-
-cd ..
